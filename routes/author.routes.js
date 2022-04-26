63de3f78d4e12b51701e2eb15c8ead707bb916fb
Ivo Silva <silvaivosoares@gmail.com>
2022-04-26 16:09:34 +0100

    Added a prevention system for removing an author and not break the books list directory

--- routes/author.routes.js
+++ routes/author.routes.js
@@ -17,7 +17,11 @@ const router = require("express").Router();
 router.get("/authors", (req, res, next) => {
 
     Author.find()
-        .then( (authorsArr) => {
+        .then((authorsArr) => {
+            // removing the unknown author from the list of authors display
+            unknownAuthorPosition = authorsArr.indexOf("6268092ebe628b0b641b22fb");
+            authorsArr.splice(unknownAuthorPosition, 1);
+
             res.render("authors/authors-list", {authors: authorsArr});
         })
         .catch( err => {
@@ -47,6 +51,23 @@ router.post("/authors/create", (req, res, next) => {
         })
 })
 
+router.get("/authors/:authorId", (req, res, next) => {
+    const id = req.params.authorId;
+    let authorName;
+    
+    Author.findById(id)
+        .then((authorDetails) => {
+            authorName = authorDetails;
+            return Book.find({author: id});
+        })
+        .then(booksArray => {
+            res.render("authors/author-details", {books: booksArray, author: authorName});
+        })
+        .catch((err) => {
+            console.log("Error getting author info from db", err);
+            next(err);
+        })
+});
 
 router.get("/authors/:authorId/edit", (req,res,next) => {
     const id = req.params.authorId;
@@ -72,7 +93,7 @@ router.post("/authors/:authorId/edit", (req, res, next) => {
 
     Author.findByIdAndUpdate(id, newDetails)
     .then((authorFromDB) => {
-        res.redirect("/authors")
+        res.redirect("authors")
     })
     .catch((err) => {
         console.log("Error updating author on db", err);
@@ -83,13 +104,20 @@ router.post("/authors/:authorId/edit", (req, res, next) => {
 
 router.post("/authors/:id/delete", (req, res, next) => {
     const id = req.params.id;
+    const unknownAuthor = "6268092ebe628b0b641b22fb";
 
     Author.findByIdAndDelete(id)
-        .then((response) => res.redirect("/authors"))
+        .then((response) => {
+            return Book.updateMany({author: id}, {author: unknownAuthor});
+        })
+        .then(() => {
+            res.redirect("/authors")
+        })
         .catch((err) => {
             console.log("Error updating author on db", err);
             next(err);
         })
+
 })
 
 
